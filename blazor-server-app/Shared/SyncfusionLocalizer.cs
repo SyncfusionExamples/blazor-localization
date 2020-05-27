143b95b4d7bbac17bc5226c73ccea1189572e2e5
Ajith R <ajithr@syncfusion.com>
2020-05-27 10:30:16 +0530

    Added Blazor Server and WebAssembly app with Localization

---
+++ blazor-server-app/Shared/SyncfusionLocalizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+namespace SyncfusionServerLocalization.Shared
+{
+    public class SyncfusionLocalizer : ISyncfusionStringLocalizer
+    {
+        // To get the locale key from mapped resources file
+        public string Get(string key)
+        {
+            return this.Manager.GetString(key);
+        }
+
+        // To access the resource file and get the exact value for locale key
+
+        public System.Resources.ResourceManager Manager
+        {
+            get
+            {
+                return SyncfusionServerLocalization.Resources.SfResources.ResourceManager;
+            }
+        }
+    }
+}
---
+++ blazor-webassembly-app/Shared/SyncfusionLocalizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace SyncfusionWasmLocalization.Shared
+{
+    public class SyncfusionLocalizer : ISyncfusionStringLocalizer
+    {
+        // To get the locale key from mapped resources file
+        public string Get(string key)
+        {
+            return this.Manager.GetString(key);
+        }
+
+        // To access the resource file and get the exact value for locale key
+
+        public System.Resources.ResourceManager Manager
+        {
+            get
+            {
+                // Replace the ApplicationNamespace with your application name.
+                return SyncfusionWasmLocalization.Resources.SfResources.ResourceManager;
+            }
+        }
+    }
+}
